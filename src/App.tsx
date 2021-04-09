b7802fe767b6bb75e00bded38dd35704c58e67b2
CameronHonis <cameron-04@hotmail.com>
2021-04-09 12:26:20 -0400

    debugging mobile

--- src/App.tsx
+++ src/App.tsx
@@ -72,6 +72,7 @@ export function App() {
   return (
     <AppContext.Provider value={{refs, state, setState}}>
       <Home />
+      <p style={{position: "absolute", top: "25vh"}}>{window.innerWidth + " " + window.innerHeight}</p>
     </AppContext.Provider>
   );
 }
